e84e753c2c124478e5d712c480f8fe4ac6424773
minkyung <minkyung7365@gmail.com>
2023-02-07 13:48:57 +0900

    NavLink 사용예제

--- example/src/App.js
+++ example/src/App.js
@@ -1,15 +1,46 @@
 import './App.css';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, NavLink } from "react-router-dom";
+import styled from '@emotion/styled';
 import Home from './pages/Home';
 import Login from './pages/Login';
+
+const activeStyle = {
+  color: 'red', 
+  TextDecoration: 'underline'
+}
+
+const deactiveStyle = {
+  color: 'black', 
+  textDecoration: 'none'
+}
+
+const Li = styled.li({
+  width: '30%', 
+  margin: '0 auto', 
+  '& a': {
+    '&.active': {
+      borderRadius: '2em', 
+      backgroundColor: 'pink'
+    }, 
+  }, 
+});
 
 function App() {
   return (
     <div className="App">
-      <ul>
-        <li><Link to="/home">Home</Link></li>
-        <li><Link to="/login">Login</Link></li>
-      </ul>
+      // activeStyle 속성
+      <p>
+        <NavLink to="/home" style={({isActive}) => {
+          return isActive ? activeStyle : deactiveStyle;
+        }}> Home</NavLink>
+      </p>
+      // activeClassName 속성
+      <p>
+        <NavLink to="/login" className={({isActive}) => {
+          return isActive ? 'orange' : '';
+        }}> Login</NavLink>
+      </p>
+      <hr />
       <Routes>
         <Route path="/home" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
