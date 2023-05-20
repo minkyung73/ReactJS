bd297030db29812f1c70a1ffc980319edbdf13c3
minkyung <minkyung7365@gmail.com>
2023-05-21 00:58:03 +0900

    코딩앙마

--- clone/src/components/UI/AuthWrapper.js
+++
@@ -1,7 +0,0 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Navigate, useLocation } from "react-router-dom";
-import { fetchUserData } from "../../store/authAction";
-
-
-
